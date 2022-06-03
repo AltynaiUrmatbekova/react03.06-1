322e5e3e0197570c4623147a7ea35817c62f2f0a
Altynai <goldmoon1090@gmail.com>
2022-06-03 16:15:38 +0300

    Added Alert component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Paragraph from "./components/Paragraph/Paragraph";
 import koalaImage from "./assets/koala.jpg";
 import karakolImage from "./assets/karakol.jpg";
 import alakolImage from "./assets/alakol.jpg";
+import Alert from "./components/Alert/Alert";
 
 function App() {
   return (
     <div className="App">
      <h1>Hello world</h1>
+
+     <Alert success>This is a sample alert</Alert>
+     <Alert danger>This is a sample alert</Alert>
 
      <Paragraph size="32px" color="tomato" background="black">This is sample paragraph </Paragraph> 
      <Paragraph size="15px" color="blue" background="pink">This is small paragraph </Paragraph> 
